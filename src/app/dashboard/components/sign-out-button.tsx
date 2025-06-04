2541a004e1a97a5953f22446cfa523c88c5bbc55
lucasptcastro <lucasptcastro@gmail.com>
2025-06-04 06:59:46 -0300

    feat: implement authentication and redirection on the dashboard page

---
+++ src/app/dashboard/components/sign-out-button.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
+export function SignOutButton() {
+  const router = useRouter();
+
+  return (
+    <Button
+      onClick={() =>
+        authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push("/authentication");
+            },
+          },
+        })
+      }
+    >
+      Sair
+    </Button>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,16 @@
-export default function DashboardPage() {
-  return <div></div>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  return <div>{session.user.email}</div>;
 }
