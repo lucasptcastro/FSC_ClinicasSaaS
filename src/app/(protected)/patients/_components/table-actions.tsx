51ac7f8e68f777fb1e646746a5e7551c92faec0c
lucasptcastro <lucasptcastro@gmail.com>
2025-06-11 08:54:10 -0300

    feat: ensure patient can be deleted

--- src/app/(protected)/patients/_components/table-actions.tsx
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -36,19 +36,19 @@ interface PatientsTableActionsProps {
 const PatientsTableActions = ({ patient }: PatientsTableActionsProps) => {
   const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
 
-  // const deletePatientAction = useAction(deletePatient, {
-  //   onSuccess: () => {
-  //     toast.success("Paciente deletado com sucesso.");
-  //   },
-  //   onError: () => {
-  //     toast.error("Erro ao deletar paciente.");
-  //   },
-  // });
+  const deletePatientAction = useAction(deletePatient, {
+    onSuccess: () => {
+      toast.success("Paciente deletado com sucesso.");
+    },
+    onError: () => {
+      toast.error("Erro ao deletar paciente.");
+    },
+  });
 
-  // const handleDeletePatientClick = () => {
-  //   if (!patient) return;
-  //   deletePatientAction.execute({ id: patient.id });
-  // };
+  const handleDeletePatientClick = () => {
+    if (!patient) return;
+    deletePatientAction.execute({ id: patient.id });
+  };
 
   return (
     <>
@@ -85,7 +85,9 @@ const PatientsTableActions = ({ patient }: PatientsTableActionsProps) => {
                 </AlertDialogHeader>
                 <AlertDialogFooter>
                   <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                  <AlertDialogAction>Deletar</AlertDialogAction>
+                  <AlertDialogAction onClick={handleDeletePatientClick}>
+                    Deletar
+                  </AlertDialogAction>
                 </AlertDialogFooter>
               </AlertDialogContent>
             </AlertDialog>
