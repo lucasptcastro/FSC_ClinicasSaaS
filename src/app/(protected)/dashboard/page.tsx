c49e44898c90e26193b16b8afc35f8266f00cf27
lucasptcastro <lucasptcastro@gmail.com>
2025-06-16 10:14:28 -0300

    feat: add top doctors list

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import { and, count, eq, gte, lte, sql, sum } from "drizzle-orm";
+import { and, count, desc, eq, gte, lte, sql, sum } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -19,6 +19,7 @@ import { auth } from "@/lib/auth";
 import { AppointmentsChart } from "./_components/appointments-chart";
 import { DatePicker } from "./_components/date-picker";
 import { StatsCards } from "./_components/stats-cards";
+import { TopDoctors } from "./_components/top-doctors";
 
 interface DashboardPageProps {
   searchParams: Promise<{
@@ -51,47 +52,74 @@ export default async function DashboardPage({
     );
   }
 
-  const [[totalRevenue], [totalAppointments], [totalPatients], [totalDoctors]] =
-    await Promise.all([
-      //  coleta os agendamentos onde a clínica é a do usuário e a data está entre as datas selecionadas
-      db
-        .select({
-          total: sum(appointmentsTable.appointmentPriceInCents),
-        })
-        .from(appointmentsTable)
-        .where(
-          and(
-            eq(appointmentsTable.clinicId, session.user.clinic.id),
-            gte(appointmentsTable.date, new Date(from)), // gte = greater than or equal to (maior ou igual a)
-            lte(appointmentsTable.date, new Date(to)), // lte = less than or equal to (menor ou igual a)
-          ),
+  const [
+    [totalRevenue],
+    [totalAppointments],
+    [totalPatients],
+    [totalDoctors],
+    topDoctors,
+  ] = await Promise.all([
+    //  coleta os agendamentos onde a clínica é a do usuário e a data está entre as datas selecionadas
+    db
+      .select({
+        total: sum(appointmentsTable.appointmentPriceInCents),
+      })
+      .from(appointmentsTable)
+      .where(
+        and(
+          eq(appointmentsTable.clinicId, session.user.clinic.id),
+          gte(appointmentsTable.date, new Date(from)), // gte = greater than or equal to (maior ou igual a)
+          lte(appointmentsTable.date, new Date(to)), // lte = less than or equal to (menor ou igual a)
         ),
-      // coleta a quantidade de agendamentos onde a clínica é a do usuário e a data está entre as datas selecionadas
-      db
-        .select({
-          total: count(),
-        })
-        .from(appointmentsTable)
-        .where(
-          and(
-            eq(appointmentsTable.clinicId, session.user.clinic.id),
-            gte(appointmentsTable.date, new Date(from)), // gte = greater than or equal to (maior ou igual a)
-            lte(appointmentsTable.date, new Date(to)), // lte = less than or equal to (menor ou igual a)
-          ),
+      ),
+    // coleta a quantidade de agendamentos onde a clínica é a do usuário e a data está entre as datas selecionadas
+    db
+      .select({
+        total: count(),
+      })
+      .from(appointmentsTable)
+      .where(
+        and(
+          eq(appointmentsTable.clinicId, session.user.clinic.id),
+          gte(appointmentsTable.date, new Date(from)), // gte = greater than or equal to (maior ou igual a)
+          lte(appointmentsTable.date, new Date(to)), // lte = less than or equal to (menor ou igual a)
         ),
-      // coleta a quantidade de pacientes onde a clínica é a do usuário
-      db
-        .select({ total: count() })
-        .from(patientsTable)
-        .where(eq(patientsTable.clinicId, session.user.clinic.id)),
-      // coleta a quantidade de médicos onde a clínica é a do usuário
-      db
-        .select({
-          total: count(),
-        })
-        .from(doctorsTable)
-        .where(eq(doctorsTable.clinicId, session.user.clinic.id)),
-    ]);
+      ),
+    // coleta a quantidade de pacientes onde a clínica é a do usuário
+    db
+      .select({ total: count() })
+      .from(patientsTable)
+      .where(eq(patientsTable.clinicId, session.user.clinic.id)),
+    // coleta a quantidade de médicos onde a clínica é a do usuário
+    db
+      .select({
+        total: count(),
+      })
+      .from(doctorsTable)
+      .where(eq(doctorsTable.clinicId, session.user.clinic.id)),
+    // coleta os 10 médicos que mais possuem agendamentos e ordena de forma decrescente
+    db
+      .select({
+        id: doctorsTable.id,
+        name: doctorsTable.name,
+        avatarImageUrl: doctorsTable.avatarImageUrl,
+        specialty: doctorsTable.specialty,
+        appointments: count(appointmentsTable.id),
+      })
+      .from(doctorsTable)
+      .leftJoin(
+        appointmentsTable,
+        and(
+          eq(appointmentsTable.doctorId, doctorsTable.id),
+          gte(appointmentsTable.date, new Date(from)),
+          lte(appointmentsTable.date, new Date(to)),
+        ),
+      )
+      .where(eq(doctorsTable.clinicId, session.user.clinic.id))
+      .groupBy(doctorsTable.id)
+      .orderBy(desc(count(appointmentsTable.id)))
+      .limit(10),
+  ]);
 
   const chartStartDate = dayjs().subtract(10, "days").startOf("day").toDate(); // coleta a data de dias 10 atrás
   const chartEndDate = dayjs().add(10, "days").endOf("day").toDate(); // coleta a data de 10 dias a frente
@@ -138,8 +166,9 @@ export default async function DashboardPage({
           totalDoctors={totalDoctors.total}
         />
 
-        <div className="grid grid-cols-[2.25fr_1fr]">
+        <div className="grid grid-cols-[2.25fr_1fr] gap-4">
           <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
+          <TopDoctors doctors={topDoctors} />
         </div>
       </PageContent>
     </PageContainer>
